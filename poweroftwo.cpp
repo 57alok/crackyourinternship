3fed12ebd80f5f24f890d9feb7cf0c541d443ff0
Alok Jha <alok072001@gmail.com>
2022-05-15 00:01:04 +0530

    11th commit

---
+++ poweroftwo.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+        else if (n == 0 || n % 2 != 0)
+        {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+    }
+};
