ffbd0ab92aa636e7a0476e25d7bf6b7bb7938958
Alok Jha <alok072001@gmail.com>
2022-05-16 16:51:08 +0530

    16 may

---
+++ RmvLLele.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        if (head == NULL)
+        {
+            return head;
+            ;
+        }
+        while (head != nullptr and head->val == val)
+        {
+            head = head->next;
+        }
+        ListNode *t1 = head;
+        while (t1 && t1->next)
+        {
+            ListNode *t2 = t1->next;
+            if (t2->val == val)
+            {
+                t1->next = t2->next;
+                t2 = t1->next;
+            }
+            else
+            {
+                t1 = t1->next;
+            }
+        }
+        return head;
+    }
+};
