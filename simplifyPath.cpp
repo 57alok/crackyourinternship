71d505aa48b39a9dd13eeab52d8886203e4bf49f
Alok Jha <alok072001@gmail.com>
2022-05-18 00:11:13 +0530

    17 may

---
+++ simplifyPath.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    string simplifyPath(string path)
+    {
+        stack<string> st;
+        string ans = "";
+        for (int i = 0; i < path.size(); i++)
+        {
+            if (path[i] == '/')
+            {
+                continue;
+            }
+            string temp = "";
+            while (i < path.size() and path[i] != '/')
+            {
+                temp += path[i];
+                ++i;
+            }
+            if (temp == ".")
+            {
+                continue;
+            }
+            else if (temp == "..")
+            {
+                if (!st.empty())
+                {
+                    st.pop();
+                }
+            }
+            else
+            {
+                st.push(temp);
+            }
+        }
+        while (!st.empty())
+        {
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+        return ans.size() == 0 ? "/" : ans;
+    }
+};
