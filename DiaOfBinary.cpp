1f6174b4f6eacb18ca472dbb4f1d2bae6964fbec
Alok Jha <alok072001@gmail.com>
2022-05-22 16:21:38 +0530

    22 may

---
+++ DiaOfBinary.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int rec(TreeNode *root, int &d)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        int ld = rec(root->left, d);
+        int rd = rec(root->right, d);
+        d = max(d, rd + ld);
+        return max(ld, rd) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int d = 0;
+        rec(root, d);
+        return d;
+    }
+};
